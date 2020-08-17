d65192072299ff23f534093baff7a451890406aa
malav-mehta <malav.mehta@hotmail.com>
2020-08-16 21:29:08 -0400

    Updates

---
+++ precache-manifest.77ba31497c0e08b93caf699a33e5731c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5237e965275aaa267bb077150fb7f67c",
+    "url": "/md-notes/index.html"
+  },
+  {
+    "revision": "b920119ac8e691eb5708",
+    "url": "/md-notes/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d33b9db6f01babbaddc4",
+    "url": "/md-notes/static/js/2.736188dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/md-notes/static/js/2.736188dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b920119ac8e691eb5708",
+    "url": "/md-notes/static/js/main.9e97c0ea.chunk.js"
+  },
+  {
+    "revision": "b8f89701f3a62d259cfa",
+    "url": "/md-notes/static/js/runtime-main.ab930b5a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/md-notes/static/media/logo.5d5d9eef.svg"
+  }
+]);
